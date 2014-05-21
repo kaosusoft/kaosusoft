57d1375674e8616c2fa6257a4c2eeaff76494b1e
kaosu <kaosu@naver.com>
2014-05-21 18:29:56 +0900

    2014-05-21 1

--- web.js
+++ web.js
@@ -1,9 +1,20 @@
-var socketio = require('socket.io')
-;
+var socketio = require('socket.io');
 var express = require('express');
 var http = require('http');
 var fs = require('fs');
 var ejs = require('ejs');
+var nodemailer = require('nodemailer');
+var crypto = require('crypto');
+var mysql = require('mysql');
+
+var client = mysql.createConnection({
+	host: 'localhost',
+	port: '3306',
+	user: 'lible',
+	password: '0123523u'
+});
+
+client.query('USE lible');
 
 var app = express();
 
@@ -19,55 +30,100 @@ app.get('/', function(request, response){
 	});
 });
 
-app.get('/upload', function(request, response){
-	fs.readFile(__dirname+'/public/upload/index.html', 'utf8', function(error, data){
+app.get('/register', function(request, response){
+	fs.readFile(__dirname+'/public/join.html', function(error, data){
 		response.send(data.toString());
 	});
 });
 
-app.post('/upload', function(request, response){
-	var comment = request.param('comment');
-	var file = request.files.image;
-	
-	if(file){
-		var name = file.name;
-		var path = file.path;
-		var outputPath = __dirname + '/public/upload/multipart/'+name;
-		fs.rename(path, outputPath, function(error){
-			response.redirect('/upload');
+app.post('/', function(request, response){
+	var body = request.body;
+	console.log(body.email+'/'+body.password);
+	response.redirect('/');
+});
+
+app.post('/register', function(request, response){
+	var body = request.body;
+	if(body.inputP == body.inputP2){
+		console.log(body.inputE+'/'+body.inputN+'/'+body.inputP+'/'+body.inputP2);
+		var smtpTransport = nodemailer.createTransport("SMTP", {
+			service: 'Gmail',
+			auth:{
+				user: 'kaosu123456@gmail.com',
+				pass: '0123523u'
+			}
+		});
+		
+		var shasum = crypto.createHash('sha1');
+		shasum.update(body.inputE+body.inputP);
+		var output = shasum.digest('hex');
+		
+		console.log(output);
+		
+		var htmlstr = '<h1>가오수월드 가입확인 메일입니다.</h1><h2>가입을 원하시는 경우 다음 링크를 클릭 해 주세요.</h2><a href="http://kaosusoft.cafe24app.com/confirm/';
+		htmlstr += output;
+		htmlstr += '">가입완료하기</a>';
+		
+		var mailOptions = {
+			from: '가오수 <kaosu@naver.com>',
+			to: body.inputE,
+			subject: '가오수 월드 가입확인 메일입니다.',
+			html: htmlstr
+		};
+		
+		smtpTransport.sendMail(mailOptions, function(error, responses){
+			if(error){
+				console.log(error);
+				response.redirect('/registererror');
+			}else{
+				console.log("Message sent : "+responses.message);
+				memberConfirm.push(new memberInfo(output, body.inputE, body.inputN, body.inputP));
+				for(var i=0; i<memberConfirm.length; i++){
+					console.log(memberConfirm[i].code + '/'+memberConfirm[i].name);
+				}
+				response.redirect('/registerok');
+			}
+			smtpTransport.close();
 		});
 	}else{
-		response.send(404);
+		console.log('틀림');
+		response.redirect('/registererror');
 	}
+	
 });
 
-app.get('/racer', function(request, response){
-	fs.readFile(__dirname+'/public/racer/index.html', 'utf8', function(error, data){
+app.get('/confirm/:token', function(request, response){
+	var token = request.param('token');
+	for(var i=0; i<memberConfirm.length; i++){
+		if(memberConfirm[i].code == token){
+			client.query('INSERT INTO member (email, name, password, token) VALUES (?, ?, ?, ?)', [memberConfirm[i].email, memberConfirm[i].name, memberConfirm[i].pw, memberConfirm[i].code], function(error, result, fields){
+				if(error){
+					console.log('fail');
+				}else{
+					console.log(result);
+				}
+			});
+			response.redirect('/registerconfirm');
+			return;
+		}
+	}
+	response.redirect('/registererror');
+});
+
+app.get('/registerconfirm', function(request, response){
+	fs.readFile(__dirname+'/public/registerconfirm.html', function(error, data){
 		response.send(data.toString());
 	});
 });
 
-app.get('/quoridor', function(request, response){
-	fs.readFile(__dirname+'/public/quoridor/index.html', 'utf8', function(error, data){
+app.get('/registerok', function(request, response){
+	fs.readFile(__dirname+'/public/registerok.html', function(error, data){
 		response.send(data.toString());
 	});
 });
 
-app.get('/quoridor_admin', function(request, response){
-	fs.readFile(__dirname+'/public/quoridor_admin/index.html', 'utf8', function(error, data){
-		response.send(data.toString());
-	});
-});
-
-
-app.get('/memorycard', function(request, response){
-	fs.readFile(__dirname+'/public/memorycard/memorycard.html', 'utf8', function(error, data){
-		response.send(data.toString());
-	});
-});
-
-app.get('/testgame', function(request, response){
-	fs.readFile(__dirname+'/public/testgame/index.html', 'utf8', function(error, data){
+app.get('/registererror', function(request, response){
+	fs.readFile(__dirname+'/public/registererror.html', function(error, data){
 		response.send(data.toString());
 	});
 });
@@ -85,5 +141,14 @@ io.sockets.on('connection', function(socket){
 		
 	});
 });
+
+function memberInfo(code, email, name, pw){
+	this.code = code;
+	this.email = email;
+	this.name = name;
+	this.pw = pw;
+}
+
+var memberConfirm = [];
 
 
