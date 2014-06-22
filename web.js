e38d6825a14baf276d605926c4e6d840608ee464
kaosu <kaosu@naver.com>
2014-06-22 14:25:22 +0900

    2014-06-22 1

--- web.js
+++ web.js
@@ -21,8 +21,6 @@ var client = mysql.createConnection({
 	// database: 'lible'
 // });
 
-// client.query('USE lible');
-
 var app = express();
 
 app.use(express.cookieParser());
@@ -52,45 +50,79 @@ app.post('/', function(request, response){
 app.post('/register', function(request, response){
 	var body = request.body;
 	if(body.inputP == body.inputP2){
-		console.log(body.inputE+'/'+body.inputN+'/'+body.inputP+'/'+body.inputP2);
-		var smtpTransport = nodemailer.createTransport("SMTP", {
-			service: 'Gmail',
-			auth:{
-				user: 'kaosu123456@gmail.com',
-				pass: '0123523u'
-			}
-		});
+		console.log(body.inputN+'/'+body.inputP+'/'+body.inputN2+'/'+body.inputE);
+		// var smtpTransport = nodemailer.createTransport("SMTP", {
+			// service: 'Gmail',
+			// auth:{
+				// user: 'kaosu123456@gmail.com',
+				// pass: '0123523u'
+			// }
+		// });
+		
+		var email = body.inputE;
+		var pw = body.inputP;
+		var name = body.inputN;
+		var nickname = body.inputN2;
 		
 		var shasum = crypto.createHash('sha1');
-		shasum.update(body.inputE+body.inputP);
+		shasum.update(body.inputP);
 		var output = shasum.digest('hex');
-		
 		console.log(output);
-		var htmlstr = '<h1>가오수월드 가입확인 메일입니다.</h1><h2>가입을 원하시는 경우 다음 링크를 클릭 해 주세요.</h2><a href="http://kaosusoft.cafe24app.com/confirm/';
-		htmlstr += output;
-		htmlstr += '">가입완료하기</a>';
 		
-		var mailOptions = {
-			from: '가오수 <kaosu@naver.com>',
-			to: body.inputE,
-			subject: '가오수 월드 가입확인 메일입니다.',
-			html: htmlstr
-		};
+		var shasum2 = crypto.createHash('sha1');
+		shasum2.update(body.inputN+body.inputP+'kaosu');
+		var output2 = shasum2.digest('hex');
 		
-		smtpTransport.sendMail(mailOptions, function(error, responses){
+		client.query('SELECT * from member where email = ?', email, function(error, result, fields){
 			if(error){
-				console.log(error);
 				response.redirect('/registererror');
 			}else{
-				console.log("Message sent : "+responses.message);
-				memberConfirm.push(new memberInfo(output, body.inputE, body.inputN, body.inputP));
-				for(var i=0; i<memberConfirm.length; i++){
-					console.log(memberConfirm[i].code + '/'+memberConfirm[i].name);
+				console.log(result.length);
+				if(result.length>0){
+					response.redirect('/registeralready');
+				}else{
+					console.log('success1');
+					client.query('INSERT INTO member (email, name, password, token, nickname) VALUES (?, ?, ?, ?, ?)', [email, name, output, output2, nickname], function(errors, results, fieldss){
+						if(errors){
+							console.log('fail');
+							console.log(errors);
+							response.redirect('/registererror');
+						}else{
+							console.log('success');
+							response.redirect('/registerok');
+						}
+					});
 				}
-				response.redirect('/registerok');
 			}
-			smtpTransport.close();
 		});
+		
+		//var htmlstr = '<h1>가오수월드 가입확인 메일입니다.</h1><h2>가입을 원하시는 경우 다음 링크를 클릭 해 주세요.</h2><a href="http://kaosusoft.cafe24app.com/confirm/';
+		
+		// var htmlstr = '<h1>가오수월드 가입확인 메일입니다.</h1><h2>가입을 원하시는 경우 다음 링크를 클릭 해 주세요.</h2><a href="http://127.0.0.1:8002/confirm/';
+		// htmlstr += output;
+		// htmlstr += '">가입완료하기</a>';
+// 		
+		// var mailOptions = {
+			// from: '가오수 <kaosu@naver.com>',
+			// to: body.inputE,
+			// subject: '가오수 월드 가입확인 메일입니다.',
+			// html: htmlstr
+		// };
+// 		
+		// smtpTransport.sendMail(mailOptions, function(error, responses){
+			// if(error){
+				// console.log(error);
+				// response.redirect('/registererror');
+			// }else{
+				// console.log("Message sent : "+responses.message);
+				// memberConfirm.push(new memberInfo(output, body.inputE, body.inputN, body.inputP));
+				// for(var i=0; i<memberConfirm.length; i++){
+					// console.log(memberConfirm[i].code + '/'+memberConfirm[i].name);
+				// }
+				// response.redirect('/registerok');
+			// }
+			// smtpTransport.close();
+		// });
 	}else{
 		console.log('틀림');
 		response.redirect('/registererror');
@@ -102,7 +134,6 @@ app.get('/confirm/:token', function(request, response){
 	var token = request.param('token');
 	for(var i=0; i<memberConfirm.length; i++){
 		if(memberConfirm[i].code == token){
-			// client.query('select * from member', function(error, result, fields){
 			client.query('INSERT INTO member (email, name, password, token) VALUES (?, ?, ?, ?)', [memberConfirm[i].email, memberConfirm[i].name, memberConfirm[i].pw, memberConfirm[i].code], function(error, result, fields){
 				if(error){
 					console.log('fail');
@@ -138,15 +169,101 @@ app.get('/registererror', function(request, response){
 	});
 });
 
+app.get('/registeralready', function(request, response){
+	fs.readFile(__dirname+'/public/registeralready.html', function(error, data){
+		response.send(data.toString());
+	});
+});
+
 var server = http.createServer(app);
 
 server.listen(8002, function(){
-	console.log('Server Running at http://kaosu.cafe24app.com');
+	console.log('Server Running at http://kaosusoft.cafe24app.com');
 });
 
 var io = socketio.listen(server);
 io.set('log level', 2);
 io.sockets.on('connection', function(socket){
+	socket.on('join_member', function(){
+		socket.join('kaosusoft');
+		socket.set('room', 'kaosusoft');
+	});
+	socket.on('name_confirms', function(data){
+		console.log(data);
+		client.query('SELECT * from member where name = ?', data, function(error, result, fields){
+			if(error){
+				console.log('fail');
+				socket.emit('name_confirm', {
+					type: 0,
+					name: data
+				});
+			}else{
+				console.log('success');
+				if(result.length>0){
+					socket.emit('name_confirm', {
+						type: 1,
+						name: data
+					});
+				}else{
+					socket.emit('name_confirm', {
+						type: 2,
+						name: data
+					});
+				}
+			}
+		});
+	});
+	socket.on('nickname_confirms', function(data){
+		console.log(data);
+		client.query('SELECT * from member where nickname = ?', data, function(error, result, fields){
+			if(error){
+				console.log('fail');
+				socket.emit('nickname_confirm', {
+					type: 0,
+					name: data
+				});
+			}else{
+				console.log('success');
+				if(result.length>0){
+					socket.emit('nickname_confirm', {
+						type: 1,
+						name: data
+					});
+				}else{
+					socket.emit('nickname_confirm', {
+						type: 2,
+						name: data
+					});
+				}
+			}
+		});
+	});
+	socket.on('email_confirms', function(data){
+		console.log(data);
+		client.query('SELECT * from member where email = ?', data, function(error, result, fields){
+			if(error){
+				console.log('fail');
+				socket.emit('email_confirm', {
+					type: 0,
+					name: data
+				});
+			}else{
+				console.log('success');
+				if(result.length>0){
+					socket.emit('email_confirm', {
+						type: 1,
+						name: data
+					});
+				}else{
+					socket.emit('email_confirm', {
+						type: 2,
+						name: data
+					});
+				}
+			}
+		});
+	});
+	
 	socket.on('disconnect', function(){
 		
 	});
