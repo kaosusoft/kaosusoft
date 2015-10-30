b727a069ae661f26b7028ab78a738ac6a0a1c777
lible <kaosu@naver.com>
2015-10-30 12:40:57 +0900

    2015-10-30 3

--- web.js
+++ web.js
@@ -11,7 +11,7 @@ var easyimage = require('easyimage');
 var uuid = require('node-uuid');
 var util = require('./util.js');
 
-var test = true; // 테스트중이면 true, 진짜는 false 
+var test = false; // 테스트중이면 true, 진짜는 false 
 
 var client;
 
@@ -40,7 +40,6 @@ app.use(app.router);
 app.use(express.static(__dirname+'/public'));
 
 app.get('/', function(request, response){
-	console.log('cookie-session : '+request.cookies.session);
 	if(request.cookies.session == undefined){
 		fs.readFile(__dirname+'/public/index.html', 'utf8',  function(error, data){
 			response.send(ejs.render(data, {
@@ -50,17 +49,13 @@ app.get('/', function(request, response){
 				}
 			}));
 		});
-	}else{
-		console.log('cookie-session : '+request.cookies.session);
-		
+	}else{		
 		client.query('SELECT * from member where token = ?', request.cookies.session, function(error, result, fields){
 			if(error){
 				response.redirect('/logout');
 			}else{
 				if(result.length>0){
 					fs.readFile(__dirname+'/public/index.html', 'utf8',  function(error, data){
-						console.log('name : '+result[0].name);
-						console.log('nickname : '+result[0].nickname);
 						response.send(ejs.render(data, {
 							data: {
 								test : test,
@@ -83,27 +78,8 @@ app.get('/logout', function(request, response){
 	response.redirect('/');
 });
 
-app.get('/register', function(request, response){
-	fs.readFile(__dirname+'/public/register.html', function(error, data){
-		response.send(data.toString());
-	});
-});
-
 app.get('/join', function(request, response){
 	fs.readFile(__dirname+'/public/join.html', function(error, data){
-		response.send(data.toString());
-	});
-});
-
-app.get('/kaosu', function (request, response) {
-	console.log('cookie : '+request.cookies.pw);
-	
-	fs.readFile(__dirname+'/public/worldcup.html', function (error, data) {
-		response.cookie('string', 'cookie');
-		response.cookie('json',{
-			id: 'kaosu',
-			pw: 'kaosu123'
-		});
 		response.send(data.toString());
 	});
 });
@@ -111,8 +87,6 @@ app.get('/kaosu', function (request, response) {
 app.get('/login_error/:id', function(request, response){
 	var id = request.param('id');
 	fs.readFile(__dirname+'/public/index_redirect.html', 'utf8', function(error, data){
-		console.log('id : '+id);
-		console.log(data);
 		response.send(ejs.render(data, {
 			data: id
 		}));
@@ -123,22 +97,17 @@ app.post('/', function(request, response){
 	var body = request.body;
 	var name = body.email;
 	var password = body.password;
-	console.log(body.email+'/'+body.password);
 	
 	client.query('SELECT * from member where name = ?', name, function(error, result, fields){
 		if(error){
 			response.redirect('/login_error/2');
 		}else{
 			if(result.length>0){
-				console.log('log : '+result[0].name);
-				console.log('log : '+result[0].password);
 				var shasum = crypto.createHash('sha1');
 				shasum.update(name+'#'+password+'@kaosu');
 				var pw = shasum.digest('hex');
-				console.log('log pw : '+pw);
 				if(pw == result[0].password){
 					var output2 = uuid.v4();
-					console.log('login success : '+result[0].id+' token : '+output2);
 					client.query('update member set token=? where id=?', [output2, result[0].id], function(){
 						response.cookie('session', output2);
 						response.redirect('/');
@@ -155,7 +124,6 @@ app.post('/', function(request, response){
 
 app.post('/join', function(request, response){
 	var body = request.body;
-	console.log(body.inputN+'/'+body.inputP+'/'+body.inputN2);
 	
 	var pw = body.inputP;
 	var name = body.inputN;
@@ -164,7 +132,6 @@ app.post('/join', function(request, response){
 	var shasum = crypto.createHash('sha1');
 	shasum.update(body.inputP+'kaosu');
 	var output = shasum.digest('hex');
-	console.log(output);
 	
 	// var shasum2 = crypto.createHash('sha1');
 	// shasum2.update(body.inputN+body.inputP+'kaosu');
@@ -175,18 +142,13 @@ app.post('/join', function(request, response){
 		if(error){
 			response.redirect('/registererror');
 		}else{
-			console.log(result.length);
 			if(result.length>0){
 				response.redirect('/registeralready');
 			}else{
-				console.log('success1');
 				client.query('INSERT INTO member (name, password, token, nickname, level, exp) VALUES (?, ?, ?, ?, ?, ?, ?)', [name, output, output2, nickname, 1, 0], function(errors, results, fieldss){
 					if(errors){
-						console.log('fail');
-						console.log(errors);
 						response.redirect('/registererror');
 					}else{
-						console.log('success');
 						response.redirect('/registerok');
 					}
 				});
@@ -195,55 +157,7 @@ app.post('/join', function(request, response){
 	});
 });
 
-app.post('/register', function(request, response){
-	var body = request.body;
-	if(body.inputP == body.inputP2){
-		console.log(body.inputN+'/'+body.inputP+'/'+body.inputN2+'/'+body.inputE);
-		// var smtpTransport = nodemailer.createTransport("SMTP", {
-			// service: 'Gmail',
-			// auth:{
-				// user: 'kaosu123456@gmail.com',
-				// pass: '0123523u'
-			// }
-		// });
-		
-		var email = body.inputE;
-		var pw = body.inputP;
-		var name = body.inputN;
-		var nickname = body.inputN2;
-		
-		var shasum = crypto.createHash('sha1');
-		shasum.update(body.inputP+'kaosu');
-		var output = shasum.digest('hex');
-		console.log(output);
-		
-		var shasum2 = crypto.createHash('sha1');
-		shasum2.update(body.inputN+body.inputP+'kaosu');
-		var output2 = shasum2.digest('hex');
-		
-		client.query('SELECT * from member where email = ?', email, function(error, result, fields){
-			if(error){
-				response.redirect('/registererror');
-			}else{
-				console.log(result.length);
-				if(result.length>0){
-					response.redirect('/registeralready');
-				}else{
-					console.log('success1');
-					client.query('INSERT INTO member (email, name, password, token, nickname, level, exp) VALUES (?, ?, ?, ?, ?, ?, ?)', [email, name, output, output2, nickname, 1, 0], function(errors, results, fieldss){
-						if(errors){
-							console.log('fail');
-							console.log(errors);
-							response.redirect('/registererror');
-						}else{
-							console.log('success');
-							response.redirect('/registerok');
-						}
-					});
-				}
-			}
-		});
-		
+// app.post('/register', function(request, response){
 		//var htmlstr = '<h1>가오수월드 가입확인 메일입니다.</h1><h2>가입을 원하시는 경우 다음 링크를 클릭 해 주세요.</h2><a href="http://kaosusoft.cafe24app.com/confirm/';
 		
 		// var htmlstr = '<h1>가오수월드 가입확인 메일입니다.</h1><h2>가입을 원하시는 경우 다음 링크를 클릭 해 주세요.</h2><a href="http://127.0.0.1:8002/confirm/';
@@ -271,57 +185,28 @@ app.post('/register', function(request, response){
 			// }
 			// smtpTransport.close();
 		// });
-	}else{
-		console.log('틀림');
-		response.redirect('/registererror');
-	}
-	
-});
+// });
 
-app.get('/confirm/:token', function(request, response){
-	var token = request.param('token');
-	for(var i=0; i<memberConfirm.length; i++){
-		if(memberConfirm[i].code == token){
-			client.query('INSERT INTO member (email, name, password, token, level, exp) VALUES (?, ?, ?, ?)', [memberConfirm[i].email, memberConfirm[i].name, memberConfirm[i].pw, memberConfirm[i].code], function(error, result, fields){
-				if(error){
-					console.log('fail');
-					console.log(error);
-				}else{
-					console.log('success');
-					console.log(result);
-				}
-				console.log('result');
-			});
-			response.redirect('/registerconfirm');
-			return;
-		}
-	}
-	response.redirect('/registererror');
-});
-
-app.get('/registerconfirm', function(request, response){
-	fs.readFile(__dirname+'/public/registerconfirm.html', function(error, data){
-		response.send(data.toString());
-	});
-});
-
-app.get('/registerok', function(request, response){
-	fs.readFile(__dirname+'/public/registerok.html', function(error, data){
-		response.send(data.toString());
-	});
-});
-
-app.get('/registererror', function(request, response){
-	fs.readFile(__dirname+'/public/registererror.html', function(error, data){
-		response.send(data.toString());
-	});
-});
-
-app.get('/registeralready', function(request, response){
-	fs.readFile(__dirname+'/public/registeralready.html', function(error, data){
-		response.send(data.toString());
-	});
-});
+// app.get('/confirm/:token', function(request, response){
+	// var token = request.param('token');
+	// for(var i=0; i<memberConfirm.length; i++){
+		// if(memberConfirm[i].code == token){
+			// client.query('INSERT INTO member (email, name, password, token, level, exp) VALUES (?, ?, ?, ?)', [memberConfirm[i].email, memberConfirm[i].name, memberConfirm[i].pw, memberConfirm[i].code], function(error, result, fields){
+				// if(error){
+					// console.log('fail');
+					// console.log(error);
+				// }else{
+					// console.log('success');
+					// console.log(result);
+				// }
+				// console.log('result');
+			// });
+			// response.redirect('/registerconfirm');
+			// return;
+		// }
+	// }
+	// response.redirect('/registererror');
+// });
 
 app.get('/worldcup', function(request, response){
 	fs.readFile(__dirname+'/public/worldcup.html', function(error, data){
@@ -395,27 +280,6 @@ app.get('/upload', function(request, response){
 		});
 	});
 });
-
-function utf16to8(str) {
-    var out, i, len, c;
-
-    out = "";
-    len = str.length;
-    for(i = 0; i < len; i++) {
-	c = str.charCodeAt(i);
-	if ((c >= 0x0001) && (c <= 0x007F)) {
-	    out += str.charAt(i);
-	} else if (c > 0x07FF) {
-	    out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
-	    out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));
-	    out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
-	} else {
-	    out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));
-	    out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
-	}
-    }
-    return out;
-}
 
 app.get('/download/:id', function(request, response){
 	client.query('select * from myfile2 where _id=?', request.param('id'), function(error, result, fields){
@@ -783,30 +647,23 @@ io.sockets.on('connection', function(socket){
 		var shasum = crypto.createHash('sha1');
 		shasum.update(name+'#'+pw+'@kaosu');
 		var output = shasum.digest('hex');
-		console.log(output);
 		
 		// var shasum2 = crypto.createHash('sha1');
 		// shasum2.update(body.inputN+body.inputP+'kaosu');
 		// var output2 = shasum2.digest('hex');
 		var output2 = uuid.v4();
-		console.log(output2);
 
 		client.query('SELECT * from member where name = ?', name, function(error, result, fields){
 			if(error){
 				socket.emit('join_error');
 			}else{
-				console.log(result.length);
 				if(result.length>0){
 					socket.emit('join_already');
 				}else{
-					console.log('success1');
 					client.query('INSERT INTO member (name, password, token, nickname, level, exp) VALUES (?, ?, ?, ?, ?, ?)', [name, output, output2, nickname, 1, 0], function(errors, results, fieldss){
 						if(errors){
-							console.log('fail');
-							console.log(errors);
 							socket.emit('join_error');
 						}else{
-							console.log('success');
 							socket.emit('join_success');
 						}
 					});
@@ -816,16 +673,13 @@ io.sockets.on('connection', function(socket){
 
 	});
 	socket.on('name_confirms', function(data){
-		console.log(data);
 		client.query('SELECT * from member where name = ?', data, function(error, result, fields){
 			if(error){
-				console.log('fail');
 				socket.emit('name_confirm', {
 					type: 0,
 					name: data
 				});
 			}else{
-				console.log('success');
 				if(result.length>0){
 					socket.emit('name_confirm', {
 						type: 1,
@@ -841,16 +695,13 @@ io.sockets.on('connection', function(socket){
 		});
 	});
 	socket.on('nickname_confirms', function(data){
-		console.log(data);
 		client.query('SELECT * from member where nickname = ?', data, function(error, result, fields){
 			if(error){
-				console.log('fail');
 				socket.emit('nickname_confirm', {
 					type: 0,
 					name: data
 				});
 			}else{
-				console.log('success');
 				if(result.length>0){
 					socket.emit('nickname_confirm', {
 						type: 1,
@@ -866,16 +717,13 @@ io.sockets.on('connection', function(socket){
 		});
 	});
 	socket.on('email_confirms', function(data){
-		console.log(data);
 		client.query('SELECT * from member where email = ?', data, function(error, result, fields){
 			if(error){
-				console.log('fail');
 				socket.emit('email_confirm', {
 					type: 0,
 					name: data
 				});
 			}else{
-				console.log('success');
 				if(result.length>0){
 					socket.emit('email_confirm', {
 						type: 1,
